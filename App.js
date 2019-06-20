76867878c0d816170c4343540cd7be5505178641
Ramesh <ramesh.eerpina@gmail.com>
2019-06-20 14:01:30 -0400

    using props (parameters) in app

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { AppRegistry, Image } from 'react-native';
 
-export default class HelloWorldApp extends Component {
-  render() {
+export default class Bananas extends Component {
+  render () {
+    let pic = {
+      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    };
     return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text>Hello, world!</Text>
-      </View>
+      <Image source = {pic} style = {{width: 400, height: 300}}/>
     );
   }
 }
+AppRegistry.registerComponent('AwesomeProject', () => Bananas);
