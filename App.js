49685f147c944231079e42e0ad633835ff0f62ec
Ramesh <ramesh.eerpina@gmail.com>
2019-06-21 09:51:13 -0400

    added flex and divided the screen into half

--- App.js
+++ App.js
@@ -41,20 +41,29 @@ export default class LotsOfGreetings extends Component {
   render () {
     return (
       <View style = {styles.container}>
-        <Text style = {this.state.customStyles}>
-          'Hello Bananas'
-        </Text>
-        <Image style={{width: 300, height: 160}}
-          source = {{uri: "https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg" }}/>
+        <View style = {styles.half1}>
+          <Text style = {this.state.customStyles}>
+            'Hello Bananas'
+          </Text>
+          <Image style={{width: 300, height: 160}}
+            source = {{uri: "https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg" }}/>
+        </View>
+        <View style = {styles.half2}></View>
       </View>
     );
   }
 }
 const styles = StyleSheet.create({
   container: {
+    flex: 1
+  },
+  half1: {
     flex: 1,
-    alignItems: 'center',
-    top: 100
+    backgroundColor: 'red',
+    alignItems: 'center'
+  },
+  half2 : {
+    flex : 1,
+    backgroundColor: "blue"
   }
-}
-) 
+}) 
