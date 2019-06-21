38a3d15f351e328db3e2d6117bd61d1f032b9d58
Ramesh <ramesh.eerpina@gmail.com>
2019-06-21 15:49:55 -0400

    basic layout of a calculator

--- App.js
+++ App.js
@@ -1,112 +1,46 @@
 import React, { Component } from 'react';
-import { AppRegistry, Text, View, StyleSheet, Image, TextInput, Button } from 'react-native';
+import {Text,View, StyleSheet} from 'react-native';
 
-/*
-class Greeting extends Component {
-  render () {
-    return (
-      <View style = {{ alignItems: 'center'}}>
-        <Text>Hello {this.props.name}!</Text>
-      </View>
-    );
-  }
-}
-*/
-export default class LotsOfGreetings extends Component {
-  
-  constructor () {
+export default class App extends Component {
+  constructor() {
     super()
-    this.state= {
-    }
-    
-    this.state.customStyles = {
-      color: 'red'
-    }
-    
-    setInterval(() => {
-      if(this.state.customStyles.color == 'red') {
-        this.setState({
-          customStyles: {
-            color:'white',
-            fontSize : 30
-          }
-        })
-      } else {
-        this.setState({
-          customStyles: {
-            color:'red'
-          }
-        })
-      }
-    }, 1000)
+    this.state = {}
   }
-
-  buttonPressed() {
-    console.log(this.state.username, this.state.password)
-  }
-
   render () {
     return (
-      <View style = {styles.container}>
-        <View style = {styles.half1}>
-          <Text style = {this.state.customStyles}>
-            'Hello Bananas'
-          </Text>
-          <Image style={{width: 300, height: 160}}
-            source = {{uri: "https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg" }}/>
-        </View>
-
-        <View style = {styles.half2}>
-
-          <View style = {[styles.half21, styles.half2x]}>
-            <Text style = {styles.text}>'This is 2-1'</Text>
-          </View>
-
-          <View style = {[styles.half22, styles.half2x]}>
-            <Text>username</Text>
-            <TextInput 
-              default value = {this.state.username}
-              onChangeText = {text => this.setState({username: text})}/>
-
-            <Text>password</Text>
-            <TextInput 
-              default value = {this.state.password}
-              onChangeText = {text => this.setState({password: text})}/> 
-            <Button title = {"Click Here"} onPress = {this.buttonPressed}/>
-          </View>
+      <View style={styles.container}>
+        <View style = {styles.result}></View>
+        <View style = {styles.calculation}></View>
+        <View style = {styles.buttons}>
+          <View style = {styles.numbers}></View>
+          <View style = {styles.operations}></View>
         </View>
       </View>
     );
   }
 }
-const styles = StyleSheet.create({
-  container: {
+const styles = StyleSheet.create ({
+  container : {
     flex: 1
   },
-  half1: {
-    flex: 1,
-    backgroundColor: 'red',
-    alignItems: 'center'
-  }, 
-  half2 : {
-    flex : 3,
-    backgroundColor: "blue",
-    flexDirection: 'row'
+  result : {
+    flexGrow : 2, 
+    backgroundColor: 'red'
   },
-  half2x: {
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  half21 : {
-    flex: 1,
-    backgroundColor: 'orange'
-  },
-  half22 : {
-    flex: 2,
+  calculation : {
+    flexGrow : 1, 
     backgroundColor: 'green'
   },
-  text: {
-    color : 'white',
-    fontSize: 20
+  buttons : { 
+    flexGrow: 7,
+    flexDirection: 'row'
+  },
+  numbers : {
+    flex: 3,
+    backgroundColor: 'yellow'
+  },
+  operations : {
+    flex: 1,
+    backgroundColor: 'black'
   }
-}) 
+})
